8924230c9d5f5dff755930fdd3a03ac88f9f0d26
NailsonChagas <58998820+NailsonChagas@users.noreply.github.com>
2019-12-20 19:13:50 -0300

    Add files via upload

---
+++ 7.1 - Outra forma de fazer(dicionario de cor).py
@@ -0,0 +1,11 @@
+nome = 'Nailson'
+
+cores = {'limpa': '\033[m',
+         'azul': '\033[34m',
+         'amarelo': '\033[33m',
+         'pretoebranco': '\033[7:30m'}
+
+print(f'Olá! Muito prazer em te conhecer, {(cores["azul"])}{nome:}!!!')
+print(f'Olá! Muito prazer em te conhecer, {(cores["amarelo"])}{nome:}!!!')
+print(f'{cores["limpa"]}')
+print(f'Olá! Muito prazer em te conhecer, {(cores["amarelo"])}{nome:}!!!')
